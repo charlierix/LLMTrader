57f8e4fdfd064cef5e9151ea2432afc7d797e084
Charlie Rix <charlie.rix@gmail.com>
2025-01-03 00:58:12 -0600

    added tester call to llm

--- LLMTrader_WPF/MarketSessionTestWindow.xaml.cs
+++ LLMTrader_WPF/MarketSessionTestWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using OllamaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,14 +25,23 @@ namespace LLMTrader_WPF
             Background = SystemColors.ControlBrush;
         }
 
-        private void Generate_MarketSessionRoot_Click(object sender, RoutedEventArgs e)
+        // TODO: once designs are more solid, move the llm callers to core project
+
+        private async void Generate_MarketSessionRoot_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                IChatCompletionService chatService = new OllamaApiClient(txtOllamaURL.Text, txtOllamaModelGenerate.Text).AsChatCompletionService();
 
+                ChatHistory chatHistory = new ChatHistory("You are a helpful assistant that knows about AI.");
+
+                chatHistory.AddUserMessage("Hi, I'm looking for book suggestions");
+
+                var reply = await chatService.GetChatMessageContentAsync(chatHistory);
             }
             catch (Exception ex)
             {
+                MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
